e9149d3780e4682f578ec670eb5b2e7bc24fca3f
Rofiqul Umma <rofiqulumma@gmail.com>
2026-06-29 14:42:35 +0700

    feat: enhance loading indicator with customizable stroke width and rounded cap

--- app/src/main/java/com/rofiq/launcherly/common/widgets/loading_indicator.kt
+++ app/src/main/java/com/rofiq/launcherly/common/widgets/loading_indicator.kt
@@ -6,6 +6,9 @@ import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.rofiq.launcherly.common.color.TVColors
 
@@ -15,9 +18,11 @@ fun LoadingIndicator(
     color: Color = TVColors.OnSurfaceSecondary,
     size: Int = 35
 ) {
+    val strokeWidthPx = with(LocalDensity.current) { 2.dp.toPx() }
     CircularWavyProgressIndicator(
         modifier = Modifier
             .size(size.dp),
-        color = color
+        color = color,
+        stroke = Stroke(width = strokeWidthPx, cap = StrokeCap.Round)
     )
 }
